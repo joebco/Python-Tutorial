cb0dfb29e173ff8f208136ae2eaec5d7ac755b67
Joe Barsugli <joe.barsugli@gmail.com>
2020-11-11 13:30:15 -0700

    Compute wind chill

--- data/mysci3.py
+++ data/mysci3.py
@@ -2,7 +2,7 @@
 # Initialize data variable
 filename = 'wxobs20170821.txt'
 
-columns = {'date':0,'time':1,'tempout':2}
+columns = {'date':0,'time':1,'tempout':2,'windspeed':7}
 types = {'tempout': float}
 data={}
 for column in columns:
@@ -28,7 +28,7 @@ print(data['tempout'])
 columns = {'date': 0, 'time': 1, 'tempout': 2}
 
 # Data types for each column (only if non-string)
-types = {'tempout': float}
+types = {'tempout': float,'windspeed': float}
 
 # Initialize my data variable
 data = {}
@@ -53,4 +53,23 @@ with open(filename, 'r') as datafile:
          data[column].append(value)
 
 # DEBUG
-print(data['tempout'])
+#  print(data['tempout'])
+
+# Compute the wind chill temperature
+def compute_windchill(t, v):
+	a = 35.74
+	b = 0.6215
+	c = 35.75
+	d = 0.4275
+
+	v16 = v ** 0.16
+	wci = a + (b * t) - (c * v16) + (d * t * v16)
+	return wci
+
+# 
+windchill = []
+for temp. windspeed in zip(data['tempout'], data['windspeed']):
+	windchill.append(compute_windchill(temp, windspeed))
+
+#DEBUG
+print(windchill)
