b0959de34518ad741cc8871a03205fc2e33beb94
Joe Barsugli <joe.barsugli@gmail.com>
2020-11-11 13:50:19 -0700

    imperfect version

--- data/mysci3.py
+++ data/mysci3.py
@@ -1,34 +1,9 @@
-# LESSON 2
-# Initialize data variable
-filename = 'wxobs20170821.txt'
-
-columns = {'date':0,'time':1,'tempout':2,'windspeed':7}
-types = {'tempout': float}
-data={}
-for column in columns:
-    data[column]=[]
-# Read the Data file
-
-with open(filename,'r') as datafile:
-    for _ in range(3):
-        datafile.readline()
-
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column,str)
-            value = t(split_line[i])
-            data[column].append(value)
-# DEBUG
-print(data['tempout'])
-
 
 # Column names and column indices to read
-columns = {'date': 0, 'time': 1, 'tempout': 2}
+columns = {'date':0,'time':1,'tempout':2,'windspeed':7,'windchill':12}
 
 # Data types for each column (only if non-string)
-types = {'tempout': float,'windspeed': float}
+types = {'tempout': float,'windspeed': float, 'windchill': float}
 
 # Initialize my data variable
 data = {}
@@ -36,7 +11,7 @@ for column in columns:
    data[column] = []
 
 # Read and parse the data file
-filename = "data/wxobs20170821.txt"
+filename = "wxobs20170821.txt"
 with open(filename, 'r') as datafile:
 
    # Read the first three lines (header)
@@ -68,8 +43,20 @@ def compute_windchill(t, v):
 
 # 
 windchill = []
-for temp. windspeed in zip(data['tempout'], data['windspeed']):
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
 	windchill.append(compute_windchill(temp, windspeed))
 
 #DEBUG
-print(windchill)
+#print(windchill)
+#for wc_data, wc_comp in zip(data['windchill'],windchill):
+#	print(f'{wc_data:.5f}    {wc_comp:.5f}    {wc_comp - wc_data:.5f}')
+print('               ORIGINAL  COMPUTED')
+print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
+print('------- ------ --------- --------- ---------')
+zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+for date,time,wc_orig,wc_comp in zip_data:
+	wc_diff = wc_orig - wc_comp
+	print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+
+
+
